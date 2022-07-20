70b3ad209d824646ee4d952b5b435f05f6cdefe8
dev1 <dev1@gmail.com>
2022-07-20 21:31:51 +0530

    changes after stash

--- s.java
+++ s.java
@@ -1,1 +1,9 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 stash in main in branch1 from branch2
+=======
+stash in main in branch1 .... stash in branch2
+>>>>>>> Stashed changes
+=======
+stash in main in branch1 .... stash in branch2
+>>>>>>> Stashed changes
